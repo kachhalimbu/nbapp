ceabfb364618084dd82a5db6cfd38f0649b3d539
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2012-04-10 18:01:58 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+	function onRequest(request, response) {
+		var pathName = url.parse(request.url).pathname;
+		console.log("Request for " + pathName + " received !!!");
+		route(handle, pathName, response, request);
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started!!!");
+}
+
+exports.start = start;
